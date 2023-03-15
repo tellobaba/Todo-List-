6f26760cbde34b211d31b1bb77f7517f6476300f
tellobaba <olatunjitello17@gmail.com>
2023-03-15 21:36:02 +0100

    Initial Commit

---
+++ Todo List/Todo_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_ListApp.swift
+//  Todo List
+//
+//  Created by Oluwatomiwa on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Todo_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
